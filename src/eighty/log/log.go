190f70d09fb4105d4abf3eb5cf22b369290bcb97
Brian Rosner <brosner@gmail.com>
2014-07-17 16:55:11 -0600

    added more log levels

--- src/eighty/log/log.go
+++ src/eighty/log/log.go
@@ -12,6 +12,19 @@ func Printf(level string, format string, v ...interface{}) {
 	logger.Printf(fmt.Sprintf("%-9s | %s", level, format), v...)
 }
 
+func Debugf(format string, v ...interface{}) {
+	Printf("DEBUG", format, v...)
+}
+
 func Infof(format string, v ...interface{}) {
 	Printf("INFO", format, v...)
+}
+
+func Warnf(format string, v ...interface{}) {
+	Printf("WARNING", format, v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	Printf("FATAL", format, v...)
+	os.Exit(1)
 }
